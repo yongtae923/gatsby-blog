7b49a378948ae6bf706a9445fa43eba0df853e5f
Yongtae Kim <yongtae7481@gmail.com>
2023-03-22 13:36:11 +0900

    Change index head title

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -191,4 +191,4 @@ const IndexPage: React.FC<PageProps> = () => {
 
 export default IndexPage
 
-export const Head: HeadFC = () => <title>Home Page</title>
+export const Head: HeadFC = () => <title>Yongtae Kim</title>
