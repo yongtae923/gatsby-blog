35f1cdb30f5f7263911e675b96b24d0663088d27
Yongtae Kim <yongtae7481@gmail.com>
2023-03-25 15:45:13 +0900

    Add tailwind css

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,7 +6,7 @@ import Layout from "../components/layout"
 const IndexPage: React.FC<PageProps> = () => {
   return (
     <Layout pageTitle="Home Page">
-      <p>I'm making this by following the Gatsby Tutorial.</p>
+      <p className="text-3xl font-bold underline">I'm making this by following the Gatsby Tutorial.</p>
     </Layout>
   )
 }
