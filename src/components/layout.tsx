184c6aa6ecef0f072b6327f249160ec8f1e99586
Yongtae Kim <yongtae7481@gmail.com>
2023-03-25 17:40:41 +0900

    Fix layout padding

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -9,7 +9,7 @@ const Layout: React.FC<LayoutProps> = ({ children }) => {
   return (
     <div className="flex flex-col min-h-screen bg-gray-50">
       <header className="bg-white border-b border-gray-200">
-        <div className="container mx-auto px-4 py-6 flex items-center justify-between">
+        <div className="container mx-auto px-10 py-10 flex items-center justify-between">
           <div className="flex items-center">
             <Link to="/" className="text-lg font-semibold text-gray-900">
               My Logo
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,9 +5,9 @@ import Layout from "../components/layout"
 const IndexPage: React.FC = () => {
   return (
     <Layout>
-      <div className="container mx-auto px-4 py-6">
+      <div className="container mx-auto px-10 py-60">
         <h1 className="text-4xl font-bold text-gray-900">
-          From brainwaves, To Innovation
+          From brainwaves<br/>To Innovation
         </h1>
         <p className="text-sm text-gray-600 mt-2">
           Yongtae, software developer
