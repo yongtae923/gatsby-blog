3328f3928275637eb892d32a13ee49f7196e50e0
Yongtae Kim <yongtae7481@gmail.com>
2023-03-25 16:10:55 +0900

    Add tailwind layout

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,24 +1,44 @@
-import * as React from 'react'
-import { Link } from 'gatsby'
+import * as React from "react"
+import { Link } from "gatsby"
 
-type LayoutProps = {
-  pageTitle: string,
+interface LayoutProps {
   children: React.ReactNode
 }
 
-const Layout: React.FC<LayoutProps> = ({ pageTitle, children }) => {
+const Layout: React.FC<LayoutProps> = ({ children }) => {
   return (
-    <div>
-      <nav>
-        <ul>
-          <li><Link to="/">Home</Link></li>
-          <li><Link to="/about">About</Link></li>
-        </ul>
-      </nav>
-      <main>
-        <h1>{pageTitle}</h1>
-        {children}
-      </main>
+    <div className="flex flex-col min-h-screen bg-gray-50">
+      <header className="bg-white border-b border-gray-200">
+        <div className="container mx-auto px-4 py-6 flex items-center justify-between">
+          <div className="flex items-center">
+            <Link to="/" className="text-lg font-semibold text-gray-900">
+              My Logo
+            </Link>
+          </div>
+          <div className="flex items-center space-x-4">
+            <Link to="/about" className="text-gray-600 hover:text-gray-900">
+              About
+            </Link>
+            <Link to="/blog" className="text-gray-600 hover:text-gray-900">
+              Blog
+            </Link>
+            <Link to="/cv" className="text-gray-600 hover:text-gray-900">
+              CV
+            </Link>
+            <Link to="/resume" className="text-gray-600 hover:text-gray-900">
+              Resume
+            </Link>
+          </div>
+        </div>
+      </header>
+      <main className="flex-grow">{children}</main>
+      <footer className="bg-gray-100 border-t border-gray-200">
+        <div className="container mx-auto px-4 py-6">
+          <p className="text-sm text-gray-600">
+            Your information goes here
+          </p>
+        </div>
+      </footer>
     </div>
   )
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,16 +1,22 @@
+import { HeadFC } from "gatsby"
 import * as React from "react"
-import { Link, HeadFC, PageProps } from "gatsby"
 import Layout from "../components/layout"
 
-
-const IndexPage: React.FC<PageProps> = () => {
+const IndexPage: React.FC = () => {
   return (
-    <Layout pageTitle="Home Page">
-      <p className="text-3xl font-bold underline">I'm making this by following the Gatsby Tutorial.</p>
+    <Layout>
+      <div className="container mx-auto px-4 py-6">
+        <h1 className="text-4xl font-bold text-gray-900">
+          From brainwaves, To Innovation
+        </h1>
+        <p className="text-sm text-gray-600 mt-2">
+          Yongtae, software developer
+        </p>
+      </div>
     </Layout>
   )
 }
 
 export default IndexPage
 
-export const Head: HeadFC = () => <title>Yongtae Kim</title>
+export const Head: HeadFC = () => <title>Index Page</title>
