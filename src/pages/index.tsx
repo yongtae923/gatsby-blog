fe45dc9e7509e112393366bf60a7474469d65d3b
Yongtae Kim <yongtae7481@gmail.com>
2023-04-19 16:42:40 +0900

    feat: Add follow button in header

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -38,6 +38,16 @@ const IndexPage: React.FC = () => {
             Yongtae.Kim
           </h1>
         </a>
+        <div className="flex flex-row items-center justify-center">
+          <button type="button" aria-label="Follow" className="flex flex-row items-center px-4 py-2 text-xs font-medium text-center text-gray-800 transition-colors duration-150 ease-in-out border border-black border-opacity-100 rounded-full">
+            <svg className="w-6 h-6" viewBox="0 0 24 24" fill="none" stroke="currentColor">
+              <path d="M2.5 19.25C2.5 15.5221 5.52208 12.5 9.25 12.5V12.5C12.9779 12.5 16 15.5221 16 19.25V19.5C16 20.6046 15.1046 21.5 14 21.5H4.5C3.39543 21.5 2.5 20.6046 2.5 19.5V19.25Z" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path>
+              <path d="M12.75 6C12.75 7.933 11.183 9.5 9.25 9.5C7.317 9.5 5.75 7.933 5.75 6C5.75 4.067 7.317 2.5 9.25 2.5C11.183 2.5 12.75 4.067 12.75 6Z" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path>
+              <path d="M19 7.5V10.5M19 10.5V13.5M19 10.5H16M19 10.5H22" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path>
+            </svg>
+            <span className="ml-2 text-sm">Follow</span>
+          </button>
+        </div>
       </header>
     </div>
   )
