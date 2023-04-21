c8e8fcabe493020ddf1682990401a7df948fd336
Yongtae Kim <yongtae7481@gmail.com>
2023-04-21 10:08:52 +0900

    feat: main 자기소개 detail

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -97,18 +97,20 @@ const IndexPage: React.FC = () => {
         </div>
       </header>
       <main>
-        <div className="relative flex items-center w-full flex-c h-96">
-          <h1 className="relative inline-block p-8 font-serif text-4xl leading-normal text-black ">
-            From Brainwaves<br/>
-            To Innovation
-          </h1>
+        <div className="relative flex items-center w-full h-96">
+          <div>
+            <h1 className="relative inline-block p-8 font-serif text-4xl font-medium leading-normal text-black ">
+              From Brainwaves<br/>
+              To Innovation
+            </h1>
+            <p className="relative inline-block pl-8 text-sm leading-5">
+              <b>김용태</b><br/>
+              개발하는 뇌공학도<br/>
+              <a href="https://ably.team/" className="underline">에이블리코퍼레이션</a> 소프트웨어 엔지니어 <small>2022 ~</small><br/>
+              <a href="https://bioeng.kaist.ac.kr/" className="underline">카이스트 바이오및뇌공학과</a> 학부과정 재학 <small>2019 ~</small><br/>
+            </p>
+          </div>
         </div>
-        <p>
-          <b>김용태</b><br/>
-          개발하는 뇌공학도<br/>
-          에이블리코퍼레이션 소프트웨어 엔지니어<br/>
-          카이스트 바이오및뇌공학과 학부과정 재학<br/>
-        </p>
         <div className="relative flex items-center w-full h-screen min-h-600 ">
           <div className="px-4">
             이것은 두번째 섹션
